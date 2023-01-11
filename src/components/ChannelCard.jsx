161f73b94b8f8f6262fb48b4c06da54105be0739
Kant <kant@mail.ru>
2023-01-11 22:16:28 +0300

    all

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail, marginTop }) => {
+  return (
+    <Box
+      sx={{
+        booxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        marginTop: marginTop,
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            diplay: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image="https://www.wdw-magazine.com/wp-content/uploads/2022/07/WDW-Magazine-What-Is-Order-66-Commander-Cody-Darth-Sidious-Palpatine-Featured-Lucasfilm.jpg"
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscribersCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscribersCount
+              ).toLocaleString()}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
+
+export default ChannelCard;
